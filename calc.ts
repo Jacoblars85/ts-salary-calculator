26c6df8aabc342038ca486f5ea9aded882ec6f5e
Jacob Larson <hockeybobber@comcast.net>
2024-10-16 22:01:01 -0500

    formatted doc

--- calc.ts
+++ calc.ts
@@ -1,30 +1,25 @@
-console.log('hello world');
+console.log("hello world");
 
-let monthlyCap : number = 0;
+let monthlyCap: number = 0;
 
 function deleteButton() {
-    console.log('trying to delete');
-    
+  console.log("trying to delete");
 }
 
 function submitButton() {
-    console.log('trying to button');
-    
-    let getFirstNameInput : any = document.getElementById('firstNameInput');
-    let getLastNameInput : any = document.getElementById('lastNameInput');
-    let getIdInput : any = document.getElementById('idInput');
-    let getTitleInput : any = document.getElementById('titleInput');
-    let getSalaryInput : any = document.getElementById('annualSalaryInput');
+  console.log("trying to button");
 
-    console.log('getFirstNameInput', getFirstNameInput);
-    
+  let getFirstNameInput: any = document.getElementById("firstNameInput");
+  let getLastNameInput: any = document.getElementById("lastNameInput");
+  let getIdInput: any = document.getElementById("idInput");
+  let getTitleInput: any = document.getElementById("titleInput");
+  let getSalaryInput: any = document.getElementById("annualSalaryInput");
 
+  console.log("getFirstNameInput", getFirstNameInput);
 
+  let tableBody: any = document.getElementById("tableBody");
 
-    let tableBody : any = document.getElementById('tableBody');
-
-    tableBody.innerHTML +=
-    ` 
+  tableBody.innerHTML += ` 
     <tr>
     <td>${getFirstNameInput}</td>
     <td>${getLastNameInput}</td>
@@ -33,17 +28,14 @@ function submitButton() {
     <td>$${getSalaryInput}</td>
     <td><button onclick='deleteButton(event)'>delete</button></td>
   </tr>
-  `
+  `;
 
-  
+  monthlyCap += getSalaryInput / 12;
 
-monthlyCap += getSalaryInput / 12;
+  // document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
 
-// document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
-
-if (monthlyCap > 20000) {
-    console.log('we spent way to much money', monthlyCap);
+  if (monthlyCap > 20000) {
+    console.log("we spent way to much money", monthlyCap);
     // document.getElementById('over-budget').style.color = "red";
-    
-}
+  }
 }
