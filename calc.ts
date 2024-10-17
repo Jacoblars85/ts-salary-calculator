3fa7d09c9faa85dd446b1c664ee8e19342ac3f48
Jacob Larson <hockeybobber@comcast.net>
2024-10-16 21:26:18 -0500

    made variables for all of the inputs

--- calc.ts
+++ calc.ts
@@ -10,5 +10,9 @@ function deleteButton() {
 function submitButton() {
     console.log('trying to button');
     
-
+    let getFirstNameInput = document.getElementById('firstNameInput');
+    let getLastNameInput = document.getElementById('lastNameInput');
+    let getIdInput = document.getElementById('idInput');
+    let getTitleInput = document.getElementById('titleInput');
+    let getSalaryInput = document.getElementById('annualSalaryInput');
 }
