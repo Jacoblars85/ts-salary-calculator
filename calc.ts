5bccf083904e932efa22b873ae66df5ba9860724
Jacob Larson <hockeybobber@comcast.net>
2024-10-18 14:27:04 -0500

    uncommented old code

--- calc.ts
+++ calc.ts
@@ -14,11 +14,11 @@ function submitButton(event) {
   event.preventDefault();
 
 //   Gets inputs
-//   let getFirstNameInput = document.getElementById("firstNameInput") as HTMLInputElement;
-//   let getLastNameInput = document.getElementById("lastNameInput") as HTMLInputElement;
-//   let getIdInput = document.getElementById("idInput") as HTMLInputElement;
-//   let getTitleInput = document.getElementById("titleInput") as HTMLInputElement;
-//   let getSalaryInput = document.getElementById("annualSalaryInput") as HTMLInputElement;
+  let getFirstNameInput = document.getElementById("firstNameInput") as HTMLInputElement;
+  let getLastNameInput = document.getElementById("lastNameInput") as HTMLInputElement;
+  let getIdInput = document.getElementById("idInput") as HTMLInputElement;
+  let getTitleInput = document.getElementById("titleInput") as HTMLInputElement;
+  let getSalaryInput = document.getElementById("annualSalaryInput") as HTMLInputElement;
 
 //   Clears input
 //   document.getElementById('firstNameInput').value = '';
