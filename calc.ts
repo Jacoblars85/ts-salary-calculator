bd2f00b21c29961874c0ef3c4a6db04da26d63ad
Jacob Larson <hockeybobber@comcast.net>
2024-10-21 21:06:11 -0500

    added more types to the inputs

--- calc.ts
+++ calc.ts
@@ -14,11 +14,11 @@ function submitButton(event) {
   event.preventDefault();
 
 //   Gets inputs
-  let getFirstNameInput = document.getElementById("firstNameInput") as HTMLInputElement;
-  let getLastNameInput = document.getElementById("lastNameInput") as HTMLInputElement;
-  let getIdInput = document.getElementById("idInput") as HTMLInputElement;
-  let getTitleInput = document.getElementById("titleInput") as HTMLInputElement;
-  let getSalaryInput = document.getElementById("annualSalaryInput") as HTMLInputElement;
+  let getFirstNameInput: any = document.getElementById("firstNameInput") as HTMLInputElement;
+  let getLastNameInput: any = document.getElementById("lastNameInput") as HTMLInputElement;
+  let getIdInput: any = document.getElementById("idInput") as HTMLInputElement;
+  let getTitleInput: any = document.getElementById("titleInput") as HTMLInputElement;
+  let getSalaryInput: any = document.getElementById("annualSalaryInput") as HTMLInputElement;
 
   console.log("getFirstNameInput", getFirstNameInput);
 
@@ -38,14 +38,14 @@ function submitButton(event) {
   `;
 
 //   calc the monthly cap
-//   monthlyCap += getSalaryInput / 12;
+  monthlyCap += getSalaryInput / 12;
 
 //   Changes the monthly cap on screen
-  // document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
+  document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
 
 //   Conditionally turns monthly cap p tag red
-//   if (monthlyCap > 20000) {
-//     console.log("we spent way to much money", monthlyCap);
-//     // document.getElementById('over-budget').style.color = "red";
-//   }
+  if (monthlyCap > 20000) {
+    console.log("we spent way to much money", monthlyCap);
+    // document.getElementById('over-budget').style.color = "red";
+  }
 }
