74c5e4a3cf85eafbf7d64cefe22001b8fbd59d85
Jacob Larson <hockeybobber@comcast.net>
2024-10-21 21:10:49 -0500

    new compiled file

--- calc.js
+++ calc.js
@@ -13,18 +13,18 @@ function submitButton(event) {
     var getIdInput = document.getElementById("idInput");
     var getTitleInput = document.getElementById("titleInput");
     var getSalaryInput = document.getElementById("annualSalaryInput");
-    console.log("getFirstNameInput", getFirstNameInput);
+    console.log("getFirstNameInput", getFirstNameInput.value);
     //   Setting the table body
     var tableBody = document.getElementById("tableBody");
     //   Adds inputs into the table
-    tableBody.innerHTML += " \n    <tr>\n    <td>".concat(getFirstNameInput, "</td>\n    <td>").concat(getLastNameInput, "</td>\n    <td>").concat(getIdInput, "</td>\n    <td>").concat(getTitleInput, "</td>\n    <td>$").concat(getSalaryInput, "</td>\n    <td><button onclick='deleteButton(event)'>delete</button></td>\n  </tr>\n  ");
+    tableBody.innerHTML += " \n    <tr>\n    <td>".concat(getFirstNameInput.value, "</td>\n    <td>").concat(getLastNameInput.value, "</td>\n    <td>").concat(getIdInput.value, "</td>\n    <td>").concat(getTitleInput.value, "</td>\n    <td>$").concat(getSalaryInput.value, "</td>\n    <td><button onclick='deleteButton(event)'>delete</button></td>\n  </tr>\n  ");
     //   calc the monthly cap
-    //   monthlyCap += getSalaryInput / 12;
+    monthlyCap += getSalaryInput / 12;
     //   Changes the monthly cap on screen
-    // document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
+    document.getElementById('over-budget').innerHTML = 'Total Monthly: $' + monthlyCap;
     //   Conditionally turns monthly cap p tag red
-    //   if (monthlyCap > 20000) {
-    //     console.log("we spent way to much money", monthlyCap);
-    //     // document.getElementById('over-budget').style.color = "red";
-    //   }
+    if (monthlyCap > 20000) {
+        console.log("we spent way to much money", monthlyCap);
+        // document.getElementById('over-budget').style.color = "red";
+    }
 }
